c34c6d42898de40c2358ddaa232d4de94dc9dc0e
Abhishek gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2023-09-21 14:31:25 +0530

    Update JwtAuthorization.java

--- api-gateway/src/main/java/org/swasth/apigateway/service/JwtAuthorization.java
+++ api-gateway/src/main/java/org/swasth/apigateway/service/JwtAuthorization.java
@@ -22,7 +22,8 @@ public class JwtAuthorization implements AuthorizationService{
     public boolean isAuthorized(ServerWebExchange exchange, Object payload, String entityType) {
         String path = exchange.getRequest().getPath().value();
         List<String> userRoles = (List) payload;
-
+        System.out.println("-----------------payload ----------------" + payload);
+        System.out.println("------------entity type ----------------" + entityType);
         if(StringUtils.equalsAnyIgnoreCase(entityType, "User")) {
             userRoles.add(entityType.toLowerCase());
         }
@@ -30,6 +31,7 @@ public class JwtAuthorization implements AuthorizationService{
         for(String role : userRoles){
             if(aclMap.containsKey(role)){
                 Acl acl = aclMap.get(role);
+                System.out.println("-------------role--------------------" + role);
                 if(acl.getPaths().stream().anyMatch(path::equalsIgnoreCase) || Utils.containsRegexPath(acl.getRegexPaths(),
                         path)){
                     return true;

d74e46a7f165267105fe38a664156b85de710b4a
Abhishek Gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2024-02-02 12:57:31 +0530

    Update App.tsx

--- self-service-portal/src/App.tsx
+++ self-service-portal/src/App.tsx
@@ -35,7 +35,7 @@ function App() {
         >  
         </Route>
         <Route 
-          path="/onboarding/register" 
+          path="/"
           element={<SignUp/>}
         >    
         </Route>
